ebda5dca89abda786a78b57dadc76408b73acda9
Peter Schuurman <psch@google.com>
2024-10-01 15:16:26 -0700

    Add support to pull NIM profiles from GCS cache

--- cloud-service-providers/google-cloud/gke/variables.tf
+++ cloud-service-providers/google-cloud/gke/variables.tf
@@ -440,6 +440,36 @@ variable "tag" {
   default     = ""
 }
 
+variable "ngc_transfer_image" {
+  type = string
+  description = "Docker image of NGC transfer container"
+  default     = "us-central1-docker.pkg.dev/psch-joonix/psch-gke-nim-deploy-demo/ngc-download-image"
+}
+
+variable "ngc_transfer_tag" {
+  type = string
+  description = "Docker repository tag of the NGC transfer container"
+  default     = "v2"
+}
+
+variable "ngc_bundle_gcs_bucket" {
+  type        = string
+  description = "GCS bucket containing NGC bucket with NIM profiles"
+  default     = ""
+}
+
+variable "ngc_bundle_filename" {
+  type        = string
+  description = "Filename containing NIM profiles from NGC"
+  default     = ""
+}
+
+variable "ngc_bundle_service_fqdn" {
+  type        = string
+  description = "FQDN of the service serving NIM bundle profiles"
+  default     = "nim-gke-gcs-signed-url-722708171432.us-central1.run.app"
+}
+
 variable "gpu_limits" {
   type        = number
   description = "GPU limits"

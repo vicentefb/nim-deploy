bcac0d3bf1ff19351136955f541808e4c721a118
Vicente Ferrara <vicenteferrara@google.com>
2025-03-06 19:57:13 +0000

    fixed wrong local ssd count

--- cloud-service-providers/google-cloud/gke/variables.tf
+++ cloud-service-providers/google-cloud/gke/variables.tf
@@ -371,7 +371,7 @@ variable "vm_gpu_spec_list" {
     g2-standard-24 = {
       accelerator_type  = "nvidia-l4"
       accelerator_count = 2
-      local_ssd_count   = 1
+      local_ssd_count   = 2
       gpu_family        = "l4"
     }
     g2-standard-32 = {
